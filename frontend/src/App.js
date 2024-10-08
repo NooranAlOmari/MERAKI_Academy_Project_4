9efe4b0275662fa6b69bfff2efd20e9e0d32a37b
NooranAlOmari <nooranalomari@gmail.com>
2024-10-08 22:17:02 +0300

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import "./App.css";
-import AddArticle from "./components/AddArticle";
+import { useState, createContext } from "react";
+import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
 
+import Login from './components/Login';
+
+export const AppContext = createContext();
 
 const App = () => {
+  
+  
+  const [token, setToken] = useState(localStorage.getItem('token')||'')
+  
   return (
+
+<AppContext.Provider value=
+    {{
+    token, setToken,
+    }}>
+
     <div className="App">
     <header className="App-header">
       <h1>App</h1>
     </header>
-    <AddArticle />
-  </div>
+    
+    </div>
+
+
+
+   <Routes>
+
+     
+
+   </Routes>
+
+</AppContext.Provider>
+  
+
   )
 }
 
@@ -18,3 +44,4 @@ const App = () => {
 
 
 export default App;
+//<Route path="/" element={<Register/>} />
